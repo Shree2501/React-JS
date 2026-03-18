9a592b9aa0d04abdf55d7df19a0de8384ce8c15e
Shree Sharma <sharmashree750@gmail.com>
2026-03-18 11:36:11 +0530

    Components

---
+++ components/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='Card'>
+        <h1>"Hello</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Quod, beatae, suscipit in earum itaque sunt eveniet eius libero debitis tempore?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
